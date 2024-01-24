c2fa1dab96ae01dd857e6b99435efa19daba59fe
宋孟林 <3111408366@qq.com>
2024-01-24 10:18:19 +0800

    completed first part of lab1

---
+++ lab1/HelloNumber.java
@@ -0,0 +1,18 @@
+package Introduction.Essentials;
+
+public class HelloNumber {
+    public static void main(String[] args){
+        int x;
+        x = 0;
+        while(x <= 10){
+            System.out.println(x);
+            x+=1;
+        }
+    }
+}
+
+/*
+1. Before java variables can be used ,they must be declared.
+2. Java variables  must have a specific type.
+3. Java variables types can't be change.
+ */
---
+++ lab1/HelloWorld.java
@@ -0,0 +1,13 @@
+package Introduction.Essentials;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, world!");
+    }
+}
+/*
+* 1. all code in java must be in class.
+* 2. we delimit the begining and end of segment of code useing {and}.
+* 3. All code in java must end in a semicolon.
+* 4. For code to run we need public static void main(String[] args)
+* */
